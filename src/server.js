02dfce1b6a316f16988c06b8d46717603b928848
SuaJeong-winter <sua.jeong.developer@gmail.com>
2026-06-14 19:07:51 +0900

    feat: 채팅룸 메시지 전달 기능

--- src/server.js
+++ src/server.js
@@ -16,13 +16,8 @@ const wsServer = SocketIO(httpServer);
 wsServer.on("connection", (socket) => {
   socket.on("enter_room", (roomName, done) => {
     done();
-    console.log(roomName);
-    console.log(socket.id);
-    // id 속성은 해당 소켓만의 고유한 값 -> 사용자 여러 명이 서버에 접속해 소켓을 형성해도 서로 구별할 수 있다는 뜻.
-    console.log(socket.rooms);
-    // rooms 속성은 소켓이 현재 어떤 룸에 있는지를 나타냄. 이때, 소켓이 접속한 방이 하나가 아닐수도 있다
     socket.join(roomName);
-    console.log(socket.rooms);
+    socket.to(roomName).emit("welcome");
   });
 });
 
